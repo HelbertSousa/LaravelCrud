17649c0b615349e0abcfe4cd0f48523d4707c7c4
Helbert <helbertsousar@gmail.com>
2022-11-13 17:06:59 -0300

    feat: adds route CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UsuariosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'web'], function () {
+	/* Return to login screen after logout.
+	Direct transitions to logout/login
+	*/
+	Route::get('/', HomeController::class . '@index');
+	Auth::routes();
+	Route::get('/home', HomeController::class . '@index');
 });
+
+Route::get('/usuarios', UsuariosController::class . '@index')->name('usuarios.list')->middleware('auth');
+Route::get('/usuarios/new', UsuariosController::class . '@new')->name('usuarios.new')->middleware('auth');
+Route::post('/usuarios/add', UsuariosController::class . '@add')->name('usuarios.add')->middleware('auth');
+Route::get('/usuarios/{id}/edit', UsuariosController::class . '@edit')->name('usuarios.edit')->middleware('auth');
+Route::post('/usuarios/update/{id}', UsuariosController::class . '@update')->name('usuarios.update')->middleware('auth');
+Route::delete('/usuarios/delete/{id}', UsuariosController::class . '@delete')->name('usuarios.delete')->middleware('auth');
